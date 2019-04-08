f768e4f46dafd1f268bea3ade6f0ed5d0d397f8a
sharath <sharathramku@umass.edu>
2019-04-08 16:24:50 -0400

    read add code

--- client/components/Settings.jsx
+++ client/components/Settings.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
 
 export class Settings extends React.Component {
     constructor(props) {
@@ -9,8 +9,33 @@ export class Settings extends React.Component {
     render() {
         return (
             <Container>
-                Settings
-            </Container>);
+                <Form>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>Change Email</Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Label>Font Size</Form.Label>
+                        <Form.Control as="select">
+                            <option>10</option>
+                            <option>12</option>
+                            <option>14</option>
+                            <option>16</option>
+                            <option>18</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlSelect2">
+                        <Form.Label>View Mode</Form.Label>
+                        <Form.Control as="select" multiple>
+                            <option>Day</option>
+                            <option>Night</option>
+                            <option>Color</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Button>Save Changes</Button>
+                </Form>
+            </Container>
+        );
     }
 
 }
