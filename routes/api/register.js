d5b632e28096d6d0e0345a6c3c872747a04c43f6
Daksh <dakshjain48@gmail.com>
2019-04-23 17:26:52 -0400

    fixed register.js

--- routes/api/register.js
+++ routes/api/register.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser');
+const models = require('../../models/User');
 
-const User = require('../../models/User');
-
+console.log("before router");
 router.post('/', (req, res) => {
-  let user = new User({username: req.body.username, password: req.body.password});
-  
+  console.log("before settings");
+  let settings = new models.Settings({ fontSize: 12, viewMode: 'day'});
+  console.log("after settings");
+  let user = new models.UserInfo({ username: req.body.username, password: req.body.password, settings: settings });
+  console.log("after user");
+
   user.save((err) => {
     if (err) return handleError(err);
   })
 
-  res.json({ message: 'successful'});
+  res.json({ message: 'successful' });
 });
 
 module.exports = router;
