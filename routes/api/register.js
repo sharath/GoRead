809ab921501228f2dd5a8e84798f01f21daf060c
Daksh <dakshjain48@gmail.com>
2019-04-23 17:27:57 -0400

    fixed register.js

--- routes/api/register.js
+++ routes/api/register.js
@@ -2,13 +2,9 @@ const express = require('express');
 const router = express.Router();
 const models = require('../../models/User');
 
-console.log("before router");
 router.post('/', (req, res) => {
-  console.log("before settings");
   let settings = new models.Settings({ fontSize: 12, viewMode: 'day'});
-  console.log("after settings");
   let user = new models.UserInfo({ username: req.body.username, password: req.body.password, settings: settings });
-  console.log("after user");
 
   user.save((err) => {
     if (err) return handleError(err);
