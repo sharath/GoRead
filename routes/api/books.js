6c31e031d226e850836f2313b95662b7fc5dc8d5
Steven Yean <syean@umass.edu>
2019-04-25 16:28:47 -0400

    Update books.js

--- routes/api/books.js
+++ routes/api/books.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const Book = require('../../models/Book');
 
+router.get('/:id', (req, res) => {
+  let id = req.params.id;
+
+  Book.findOne({'_id': id})
+    .then(book => res.json(book));
+});
+
 router.get('/', (req, res) => {
   Book.find()
     .then(books => res.json(books))
